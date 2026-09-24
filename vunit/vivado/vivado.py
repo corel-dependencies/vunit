7f6e8ee62b909ef039178b7dadef2653945541be
nicods <nico.desimone@desy.de>
2026-09-24 11:27:35 +0200

    vivado.py: sort libraries for compile order

--- vunit/vivado/vivado.py
+++ vunit/vivado/vivado.py
@@ -112,7 +112,7 @@ def _read_compile_order(file_name, fail_on_non_hdl_files):
             else:
                 compile_order.append((library_name, file_name))
 
-    return compile_order, libraries, list(sorted(include_dirs))
+    return compile_order, sorted(libraries), list(sorted(include_dirs))
 
 
 def run_vivado(tcl_file_name, tcl_args=None, cwd=None, vivado_path=None):
